7b104342fca60478211b31aedc056f2992e621da
LauraPereyra <laurapereyra2504@gmail.com>
2017-05-14 17:53:23 -0400

    template
    Nuevo template

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,31 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Material Admin - Blank page</title>
 
-    <!-- BEGIN META -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="your,keywords">
-    <meta name="description" content="Short explanation about this website">
-    <!-- END META -->
+    <!-- Title -->
+    <title>Alpha | Responsive Admin Dashboard Template</title>
 
-    <!-- BEGIN STYLESHEETS -->
-    <link href='http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
-    <link type="text/css" rel="stylesheet" href="{{URL::asset('/assets/css/theme-default/bootstrap.css?1422792965')}}"/>
-    <link type="text/css" rel="stylesheet" href="{{URL::asset('/assets/css/theme-default/materialadmin.css?1425466319')}}" />
-    <link type="text/css" rel="stylesheet" href="{{URL::asset('/assets/css/theme-default/font-awesome.min.css?1422529194')}}" />
-    <link type="text/css" rel="stylesheet" href="{{URL::asset('/assets/css/theme-default/material-design-iconic-font.min.css?1421434286')}}" />
-    <link type="text/css" rel="stylesheet" href="{{URL::asset('/assets/css/theme-default/libs/DataTables/jquery.dataTables.css?1423553989')}}" />
-    <link type="text/css" rel="stylesheet" href="{{URL::asset('/assets/css/theme-default/libs/DataTables/extensions/dataTables.colVis.css?1423553990')}}" />
-    <link type="text/css" rel="stylesheet" href="{{URL::asset('/assets/css/theme-default/libs/DataTables/extensions/dataTables.tableTools.css?1423553990')}}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta charset="UTF-8">
+    <meta name="description" content="Responsive Admin Dashboard Template" />
+    <meta name="keywords" content="admin,dashboard" />
+    <meta name="author" content="Steelcoders" />
+
+    <!-- Styles -->
+    <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css"/>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="assets/plugins/metrojs/MetroJs.min.css" rel="stylesheet">
+    <link href="assets/plugins/weather-icons-master/css/weather-icons.min.css" rel="stylesheet">
 
 
+    <!-- Theme Styles -->
+    <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
 
-    <!-- END STYLESHEETS -->
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script type="text/javascript" src="../../assets/js/libs/utils/html5shiv.js?1403934957"></script>
-    <script type="text/javascript" src="../../assets/js/libs/utils/respond.min.js?1403934956"></script>
+    <script src="http://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="http://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
 </head>
+
 @yield('content')
