daf7bdac868de2ad95b2db6af23c0fd738f6906d
Waskar <waskar007@gmail.com>
2017-05-23 21:11:20 -0400

    Habitaciones
    URL de las habitaciones ojala leas joel

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -10,12 +10,6 @@ class RoomController extends Controller
     public function index()
     {
 
-        /*$rooms = DB::table('rooms')
-            ->join('room_types', 'room_types.id_room_type', '=', 'rooms.room_type_id')
-            ->orderBy('id_room','asc')
-            ->paginate(10);
-
-        return view('room.room',compact('rooms'));*/
         $search = \Request::get('search');
         $rooms = DB::table('rooms')
             ->join('room_types', 'room_types.id_room_type', '=', 'rooms.room_type_id')
