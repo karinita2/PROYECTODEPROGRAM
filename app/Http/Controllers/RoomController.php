b6d3ec34482b386cd55c8a82854608e8157162c6
saulquiroz <saul85dt@gmail.com>
2017-06-09 19:59:34 -0400

    Habitaciones con todos los datos
    Compelatado jalando con leftjoins (idea del waskar)
    //Waskar Gomes
    //Saul Quiroz

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -11,16 +11,19 @@ class RoomController extends Controller
     {
 
         /*Conectando las tablas de rooms, room_types, reservations, details para sacar detalles de la habitacion*/
-        /*$search = \Request::get('search');
+        $search = \Request::get('search');
         $rooms = DB::table('rooms')
-            ->join('room_types', 'room_types.id_room_type', '=', 'rooms.room_type_id')
-            ->join('details','rooms.id_room','=','details.room_id')
-            ->join('reservations','reservations.id_reservation','=','details.reservation_id')
+            ->leftjoin('room_types', 'room_types.id_room_type', '=', 'rooms.room_type_id')
+            ->leftjoin('details','rooms.id_room','=','details.room_id')
+            ->leftjoin('reservations','reservations.id_reservation','=','details.reservation_id')
             ->where('rooms.name','like','%'.$search.'%')
             ->orWhere('room_types.room_type','like','%'.$search.'%')
             ->orWhere('rooms.availability','like','%'.$search.'%')
             ->orderBy('id_room','asc')
-            ->paginate(10);*/
+            ->paginate(10);
+
+
+        /*
         $search = \Request::get('search');
         $rooms = DB::table('rooms')
             ->join('room_types', function ($join){
@@ -30,6 +33,8 @@ class RoomController extends Controller
             })
 
             ->paginate(10);
+
+        */
         return view('room.room',compact('rooms'));
     }
     public function create()
